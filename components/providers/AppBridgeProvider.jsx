2e1275e69323ca54934a75d6c5ebe6408bf6a030
Kharazian <AmirKharazian@gmail.com>
2024-03-26 17:19:17 -0400

    fix: error for checking window

--- components/providers/AppBridgeProvider.jsx
+++ components/providers/AppBridgeProvider.jsx
@@ -1,10 +1,16 @@
-const AppBridgeProvider = ({ children }) => {
-  if (typeof window !== "undefined") {
-    const shop = window?.shopify?.config?.shop;
+import { useEffect, useState } from 'react';
 
-    if (!shop) {
-      return <p>No Shop Provided</p>;
+const AppBridgeProvider = ({ children }) => {
+  const [shop, setShop] = useState(null);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const shopFromWindow = window?.shopify?.config?.shop;
+      setShop(shopFromWindow);
     }
+  }, []);
+
+  if (!shop) {
+    return <p>No Shop Provided</p>;
   }
 
   return <>{children}</>;
