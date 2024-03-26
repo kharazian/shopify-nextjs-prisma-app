1b4cfce635b59fe46f204217cf422be77dc9635e
Kharazian <AmirKharazian@gmail.com>
2024-03-26 17:41:31 -0400

    build: change extension folder

--- _developer/tomlWriter.js
+++ _developer/tomlWriter.js
@@ -24,7 +24,7 @@ try {
   config.client_id = process.env.SHOPIFY_API_KEY;
   config.application_url = appUrl;
   config.embedded = true;
-  config.extension_directories = ["extension/extensions/*"];
+  // config.extension_directories = ["extension/extensions/*"];
 
   // Auth
   config.auth = {};
